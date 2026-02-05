a0bc53132abb1579b2535e4a07d52c629a8965c4
Heiko <789788+HeikoBornholdt@users.noreply.github.com>
2026-02-05 09:38:43 +0100

    Update Netsody Beta version to 1.0.0-beta.4

--- Formula/netsody-beta.rb
+++ Formula/netsody-beta.rb
@@ -1,33 +1,33 @@
 class NetsodyBeta < Formula
   desc "Netsody provides secure, software-defined overlay networks, connecting all your devices"
   homepage "https://netsody.io"
-  version "1.0.0-beta.1"
+  version "1.0.0-beta.4"
 
   conflicts_with cask: "drasyl-ui"
   conflicts_with formula: ["drasyl", "netsody"]
 
   on_macos do
     on_arm do
-      url "https://download.netsody.io/binaries/macos-arm64/1.0.0-beta.1/netsody"
+      url "https://download.netsody.io/binaries/macos-arm64/1.0.0-beta.4/netsody"
     end
 
     on_intel do
-      url "https://download.netsody.io/binaries/macos-amd64/1.0.0-beta.1/netsody"
+      url "https://download.netsody.io/binaries/macos-amd64/1.0.0-beta.4/netsody"
     end
   end
 
   on_linux do
     on_intel do
       if Hardware::CPU.is_64_bit?
-        url "https://download.netsody.io/binaries/linux-amd64/1.0.0-beta.1/netsody"
+        url "https://download.netsody.io/binaries/linux-amd64/1.0.0-beta.4/netsody"
       end
     end
 
     on_arm do
       if Hardware::CPU.is_64_bit?
-        url "https://download.netsody.io/binaries/linux-arm64/1.0.0-beta.1/netsody"
+        url "https://download.netsody.io/binaries/linux-arm64/1.0.0-beta.4/netsody"
       else
-        url "https://download.netsody.io/binaries/linux-armv7/1.0.0-beta.1/netsody"
+        url "https://download.netsody.io/binaries/linux-armv7/1.0.0-beta.4/netsody"
       end
     end
   end
