14267866667de4babd8abd27bfeaa5ace13fa9c9
Heiko Bornholdt <heiko@bornholdt.io>
2026-05-05 14:23:15 +0200

    1.0.0-beta.9

--- Formula/netsody-beta.rb
+++ Formula/netsody-beta.rb
@@ -1,33 +1,33 @@
 class NetsodyBeta < Formula
   desc "Netsody provides secure, software-defined overlay networks, connecting all your devices"
   homepage "https://netsody.io"
-  version "1.0.0-beta.8"
+  version "1.0.0-beta.9"
 
   conflicts_with cask: "drasyl-ui"
   conflicts_with formula: ["drasyl", "netsody"]
 
   on_macos do
     on_arm do
-      url "https://download.netsody.io/binaries/macos-arm64/1.0.0-beta.8/netsody"
+      url "https://download.netsody.io/binaries/macos-arm64/1.0.0-beta.9/netsody"
     end
 
     on_intel do
-      url "https://download.netsody.io/binaries/macos-amd64/1.0.0-beta.8/netsody"
+      url "https://download.netsody.io/binaries/macos-amd64/1.0.0-beta.9/netsody"
     end
   end
 
   on_linux do
     on_intel do
       if Hardware::CPU.is_64_bit?
-        url "https://download.netsody.io/binaries/linux-amd64/1.0.0-beta.8/netsody"
+        url "https://download.netsody.io/binaries/linux-amd64/1.0.0-beta.9/netsody"
       end
     end
 
     on_arm do
       if Hardware::CPU.is_64_bit?
-        url "https://download.netsody.io/binaries/linux-arm64/1.0.0-beta.8/netsody"
+        url "https://download.netsody.io/binaries/linux-arm64/1.0.0-beta.9/netsody"
       else
-        url "https://download.netsody.io/binaries/linux-armv7/1.0.0-beta.8/netsody"
+        url "https://download.netsody.io/binaries/linux-armv7/1.0.0-beta.9/netsody"
       end
     end
   end
